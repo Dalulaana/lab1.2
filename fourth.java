680e93895b7714a1ed2a672e0b0d8210536e2cfd
Dalulaana <daianakv12@gmail.com>
2023-07-30 18:29:21 +0600

    factorial

---
+++ fourth.java
@@ -0,0 +1,16 @@
+public class fourth
+{
+    public static int factorial(int n)
+    {
+        if (n==0||n==1)
+        { return 1; }
+        else
+        { return n*factorial(n- 1); }
+    }
+    public static void main(String[] args)
+    {
+        int n=5;
+        int result=factorial(n);
+        System.out.println(n+"! "+result);
+    }
+}
